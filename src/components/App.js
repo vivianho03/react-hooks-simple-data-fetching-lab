f208a49d9117a0d9a65cd665a5661cf7fe17b214
Vivian Ho <ho.vivian89@gmail.com>
2023-02-03 22:36:33 -0800

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+
+function FoxImage() {
+  const [image, setImage] = useState(null)
+  
+  useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => setImage(data.message))
+  }, [])
+  
+  if (!image) {
+      return <p>Loading...</p>
+  }
+  
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default FoxImage
